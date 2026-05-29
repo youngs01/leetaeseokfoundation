4fa9812b36f1de4f8e07b0e07d58bf256fa35c21
youngs01 <sinhan2023@gmail.com>
2026-05-29 14:34:17 +0900

    Delete app directory

--- app/auth/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function AuthLoginPage() {
-  redirect("/admin/login")
-}
--- app/login/admin/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function LoginAdminRedirect() {
-  // Support legacy/alternate path `/login/admin` by redirecting to canonical `/admin/login`
-  redirect("/admin/login")
-}
